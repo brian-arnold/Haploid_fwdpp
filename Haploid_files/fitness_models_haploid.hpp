f3954ee7149a9905f86f1af391276d3c0e7a03f1
Brian John Arnold <bjarnold@dhcp-10-250-252-141.harvard.edu>
2019-09-27 16:08:56 -0400

    adding updated files

--- Haploid_files/fitness_models_haploid.hpp
+++ Haploid_files/fitness_models_haploid.hpp
@@ -32,6 +32,23 @@ struct multiplicative_negseln_haploid
     }
 };
 
+struct multiplicative_posseln_haploid
+{
+    
+    template<typename gamete_type, typename mtype>
+    // function has to be const! in template declaration, functor labelled as const
+    // thus, any attempt to change a member variable or call non-const member function
+    // results in compiler error
+    inline double
+    operator()(const gamete_type &g, const std::vector<mtype> &mutations, const std::vector<uint_t> &mcounts) const noexcept
+    {
+        double product = 1.0 ;
+        for(const std::uint32_t &key : g.smutations){
+            product *= (1.0 + mutations[key].s) ;
+        }
+        return std::max(0., product);
+    }
+};
 
 class quadratic_synepistasis_negseln
 {
@@ -59,6 +76,72 @@ public:
         }
         //fitness = 1.0 - (s*muts) - b*(muts*(muts-1.0)/2.0) ;
         fitness = exp(-(s*muts) - b*(muts*(muts-1.0)/2.0)) ;
+        //std::cout << fitness << "\n" ;
+        return std::max(0., fitness);
+    }
+};
+
+class antagonistic_epistasis_negseln
+{
+private:
+    double b ; // pairwise epistasis coefficient
+    double s ; // selection coefficient
+public:
+    // Constructor
+    explicit antagonistic_epistasis_negseln(const double sel, const double beta) :
+            s(sel), b(beta)
+    {
+    }
+    
+    template<typename gamete_type, typename mtype>
+    // function has to be const! in template declaration, functor labelled as const
+    // thus, any attempt to change a member variable or call non-const member function
+    // results in compiler error
+    inline double
+    operator()(const gamete_type &g, const std::vector<mtype> &mutations, const std::vector<uint_t> &mcounts) const noexcept
+    {
+        double muts = 0.0 ;
+        double fitness ;
+        for(const std::uint32_t &key : g.smutations){
+            muts += 1.0 ;
+        }
+	// From Desai et al 2007, Genetics
+	// form: e^(-s*muts^(1-b))
+	// negative b (or minus a positive b) corresponds to antagonistic epistasis
+        fitness = exp( pow(-(s*muts), (1.0-b)) ) ;
+        //std::cout << fitness << "\n" ;
+        return std::max(0., fitness);
+    }
+};
+
+class synergistic_epistasis_posseln
+{
+private:
+    double b ; // pairwise epistasis coefficient
+    double s ; // selection coefficient
+public:
+    // Constructor
+    explicit synergistic_epistasis_posseln(const double sel, const double beta) :
+            s(sel), b(beta)
+    {
+    }
+    
+    template<typename gamete_type, typename mtype>
+    // function has to be const! in template declaration, functor labelled as const
+    // thus, any attempt to change a member variable or call non-const member function
+    // results in compiler error
+    inline double
+    operator()(const gamete_type &g, const std::vector<mtype> &mutations, const std::vector<uint_t> &mcounts) const noexcept
+    {
+        double muts = 0.0 ;
+        double fitness ;
+        for(const std::uint32_t &key : g.smutations){
+            muts += 1.0 ;
+        }
+	// From Desai et al 2007, Genetics
+	// form: e^(s*muts^(1+b))
+	// positive b corresponds to synergistic epistasis
+        fitness = exp( pow((s*muts), (1.0+b)) ) ;
         //std::cout << fitness << "\n" ;
         return std::max(0., fitness);
     }
@@ -118,6 +201,42 @@ struct pop_sign_seln_multiplicative
     }
 };
 
+class pop_sign_seln_epistasis
+{
+private:
+    double b ; // pairwise epistasis coefficient
+    double s ; // selection coefficient
+public:
+    // Constructor
+    explicit pop_sign_seln_epistasis(const double sel, const double beta) :
+    s(sel), b(beta)
+    {
+    }
+    
+    template<typename gamete_type, typename mtype>
+    // function has to be const! in template declaration, functor labelled as const
+    // thus, any attempt to change a member variable or call non-const member function
+    // results in compiler error
+    inline double
+    operator()(const gamete_type &g, const std::vector<mtype> &mutations, const int deme) const noexcept
+    {
+        double muts = 0.0 ;
+        double fitness ;
+        for(const std::uint32_t &key : g.smutations){
+            muts += 1.0 ;
+        }
+        //fitness = 1.0 - (s*muts) - b*(muts*(muts-1.0)/2.0) ;
+	if(deme){
+        	fitness = exp(-(s*muts) - b*(muts*(muts-1.0)/2.0)) ;
+	}else{
+        	fitness = exp((s*muts) + b*(muts*(muts-1.0)/2.0)) ;
+	}
+        //std::cout << fitness << "\n" ;
+        return std::max(0., fitness);
+    }
+};
+
+
 
 struct pop_specific_seln_multiplicative
 {
@@ -140,7 +259,7 @@ struct pop_specific_seln_multiplicative
 
 struct pop_pos_seln_multiplicative
 {
-    // demes have opposite selection pressures, positive in deme0, negative in deme1
+    // both demes under multiplicative positive selection
     template<typename gamete_type, typename mtype>
     inline double
     operator()(const gamete_type &g, const std::vector<mtype> &mutations, const int deme) const noexcept
@@ -148,6 +267,21 @@ struct pop_pos_seln_multiplicative
         double product = 1.0 ;
         for(const std::uint32_t &key : g.smutations){
             product *= (1.0 + mutations[key].s) ;
+        }
+        return std::max(0., product);
+    }
+};
+
+struct pop_neg_seln_multiplicative
+{
+    // both demes under multiplicative negative selection
+    template<typename gamete_type, typename mtype>
+    inline double
+    operator()(const gamete_type &g, const std::vector<mtype> &mutations, const int deme) const noexcept
+    {
+        double product = 1.0 ;
+        for(const std::uint32_t &key : g.smutations){
+            product *= (1.0 - mutations[key].s) ;
         }
         return std::max(0., product);
     }
